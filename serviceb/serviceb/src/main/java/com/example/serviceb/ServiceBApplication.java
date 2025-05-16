ba30454b9f3a9d4532bd02ad50fb810a4bb5245d
aman <amanavelon@gmail.com>
2025-05-16 13:28:42 +0530

    eureka wokring

--- serviceb/serviceb/src/main/java/com/example/serviceb/ServiceBApplication.java
+++ serviceb/serviceb/src/main/java/com/example/serviceb/ServiceBApplication.java
@@ -1,4 +1,4 @@
-package com.spriing.demo;
+package com.example.serviceb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class DemoApplication {
+public class ServiceBApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ServiceBApplication.class, args);
 	}
 
 }
