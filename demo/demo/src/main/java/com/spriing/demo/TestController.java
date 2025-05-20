a0c3659162786915cae14577f482317d25b2db80
amanavelon <amanavelon@gmail.com>
2025-05-20 21:46:34 +0530

    changes

--- demo/demo/src/main/java/com/spriing/demo/TestController.java
+++ demo/demo/src/main/java/com/spriing/demo/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello from API Gateway!";
+        return "Hello from API demo!";
     }
 }
